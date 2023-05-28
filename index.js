94b16436ef3ebc005a75dad7c57de12985759222
parvezaman <emailtoamanullah@gmail.com>
2023-05-28 16:27:42 +0600

    added phaser and showed static files to the server

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 5000;
 
+app.use(express.static(__dirname + '/public/')) // telling express where our static files are
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + 'index.html') // sending index.html to run
 })
 
 app.listen(port, () => {
---
+++ public/js/snakeGame.js
@@ -0,0 +1,9 @@
+const config = {
+    height: 640,
+    width: 640,
+    type: Phaser.AUTO,
+    parent: 'snake-game',
+    scene: []
+}
+
+new Phaser.Game(config)
