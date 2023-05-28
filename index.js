825a91103d67265e9eb86d930d65f4f235d2976a
parvezaman <emailtoamanullah@gmail.com>
2023-05-28 16:13:19 +0600

    send file in the serve

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 app.listen(port, () => {
